6d96f237c0bf3113222757e240fc487465115c61
SmitParekh84 <smartparekh02@gmail.com>
2024-05-29 02:08:48 +0530

    vercel

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import mailController from './controller/mailController.js'; // Add .js extensio
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 dotenv.config();
 
 
